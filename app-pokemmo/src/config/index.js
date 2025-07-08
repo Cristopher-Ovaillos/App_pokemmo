8cf11f390fb8125c9be289574aa389e4ac8f84b0
Cristopher-Ovaillos <cristopher.ovaillos@est.fi.uncoma.edu.ar>
2025-07-08 17:40:40 -0300

    config

--- app-pokemmo/src/config/index.js
+++ app-pokemmo/src/config/index.js
@@ -1,4 +1,4 @@
-const LOCAL_IP = '192.168.1.71';  //insert IP, en la terminal cmd de windows insert ipconfig (copiar la ip de direccion ipv4)
+const LOCAL_IP = 'tu ip';  //insert IP, en la terminal cmd de windows insert ipconfig (copiar la ip de direccion ipv4)
 
 export const API_URL = `http://${LOCAL_IP}:5000`;
 export const IMAGE_URL = `${API_URL}/images`;
